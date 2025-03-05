dc0e88f2f1c85211606c7f00168a2848f61dee1b
Parth Patel <parth.patel.reversebits@gmail.com>
2025-03-05 16:10:11 +0530

    ✨feature: Added JS Validations

--- js/validation.js
+++ js/validation.js
@@ -1,1 +1,29 @@
 localStorage.setItem("email", "parth.patel.reversebits@gmail.com");
+localStorage.setItem("password", "reverseBits");
+
+const userValidation = () => {
+  let flag = null;
+  let emailAddress = document.getElementById("form3Example3");
+  let password = document.getElementById("form3Example4");
+
+  if (
+    emailAddress.value == localStorage.getItem("email") &&
+    password.value == localStorage.getItem("password")
+  ) {
+    flag = true;
+    if (flag) {
+      document.getElementById("success-validation-msg").style.display = "block";
+      document.getElementById("invalid-validation-msg").style.display = "none";
+    } else {
+      document.getElementById("success-validation-msg").style.display = "none";
+    }
+  } else {
+    flag = false;
+
+    if (flag == false) {
+      document.getElementById("invalid-validation-msg").style.display = "block";
+    } else {
+      document.getElementById("invalid-validation-msg").style.display = "none";
+    }
+  }
+};
